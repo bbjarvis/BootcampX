2d9cac51e93138c14373f3da4ad8f1859b5fc382
bbjarvs <brettbjarvis@gmail.com>
2021-11-03 22:33:24 +0000

    add bootcamp_app to run through node postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const input = process.argv.slice(2);
+const cohort = input[0];
+const limit = input[1];
+
+pool.query(`
+SELECT students.id as id, students.name as name, cohorts.name as cohort
+FROM students
+LEFT JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [`%${cohort}%`, limit])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const input = process.argv.slice(2);
+const cohort = input[0];
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`, [`%${cohort}%`])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
+
